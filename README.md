dc398089f25c1287e55c1d17afc2c60006636c6a
akmal4896 <akmalboss007@email.arizona.edu>
2020-08-10 07:51:59 +0800

    Finalize

--- README.md
+++ README.md
@@ -66,3 +66,9 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+Libaries, dependencies and framework used:
+material UI core
+material UI Icon
+react-ticker
+firebase
---
+++ build/precache-manifest.ef2864c6be36e7694e88b376e93cc0a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc69447987e426b96b16f8301e697a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4943de0577262c63342",
+    "url": "/static/css/main.b44fb6ae.chunk.css"
+  },
+  {
+    "revision": "11f2a6012d445709d957",
+    "url": "/static/js/2.123dc598.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.123dc598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4943de0577262c63342",
+    "url": "/static/js/main.116762c0.chunk.js"
+  },
+  {
+    "revision": "3585d1ca8a2701dd5ecc",
+    "url": "/static/js/runtime-main.8db828ad.js"
+  }
+]);
